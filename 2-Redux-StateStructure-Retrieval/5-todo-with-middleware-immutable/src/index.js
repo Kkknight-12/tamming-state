0ea23c1a3a9c930df055c66ceb9d170113f4f90f
Knight <mayanks365@gmail.com>
2021-05-15 08:44:56 +0530

    updating todo with middleware, immutable

--- 2-Redux-StateStructure-Retrieval/5-todo-with-middleware-immutable/src/index.js
+++ 2-Redux-StateStructure-Retrieval/5-todo-with-middleware-immutable/src/index.js
@@ -19,16 +19,39 @@ const todos = [
   { id: '10', name: 'Hacker News with Redux' },
 ];
 
-// schemas
+// //////////
+// schemas //
+// //////////
 const todoSchema = new schema.Entity( 'todo' );
 
 const normalizedTodos = normalize( todos, [todoSchema] );
-console.log(normalizedTodos);
+// console.log(normalizedTodos);
+/*  
+{entities: {…}, result: Array(10)}
+entities:
+  todo:
+    1: {id: "1", name: "Redux Standalone with advanced Actions"}
+    2: {id: "2", name: "Redux Standalone with advanced Reducers"}
+    3: {id: "3", name: "Bootstrap App with Redux"}
+    4: {id: "4", name: "Naive Todo with React and Redux"}
+    5: {id: "5", name: "Sophisticated Todo with React and Redux"}
+    6: {id: "6", name: "Connecting State Everywhere"}
+    7: {id: "7", name: "Todo with advanced Redux"}
+    8: {id: "8", name: "Todo but more Features"}
+    9: {id: "9", name: "Todo with Notifications"}
+    10: {id: "10", name: "Hacker News with Redux"}
+result: (10) ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+*/
 
 const initialTodoState = {
   entities: normalizedTodos.entities.todo,
   ids: normalizedTodos.result,
 };
+
+// action types
+const TODO_ADD = 'TODO_ADD';
+const TODO_TOGGLE = 'TODO_TOGGLE';
+const FILTER_SET = 'FILTER_SET';
 
 // //////////
 // reducer //
@@ -44,6 +67,7 @@ function todoReducer(state = initialTodoState, action) {
     default : return state;
   }
 }
+
 function applyAddTodo( state, action ){
   const todo = { ...action.todo, completed: false };
   const entities = { ...state.entities, [todo.id]:todo };
@@ -52,10 +76,33 @@ function applyAddTodo( state, action ){
 };
 
 function applyToggleTodo( state, action ){
+  console.log(state)
+  /* 
+  entities:
+    1: {id: "1", name: "Redux Standalone with advanced Actions"}
+    2: {id: "2", name: "Redux Standalone with advanced Reducers"}
+    3: {id: "3", name: "Bootstrap App with Redux"}
+    4: {id: "4", name: "Naive Todo with React and Redux"}
+    5: {id: "5", name: "Sophisticated Todo with React and Redux"}
+    6: {id: "6", name: "Connecting State Everywhere"}
+    7: {id: "7", name: "Todo with advanced Redux"}
+    8: {id: "8", name: "Todo but more Features"}
+    9: {id: "9", name: "Todo with Notifications"}
+    10: {id: "10", name: "Hacker News with Redux"}
+  ids: (10) ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+  */
+  // console.log(action)
+  /* 
+    todo: {id: "1"}
+    type: "TODO_TOGGLE"
+  */
   const id = action.todo.id;
+  // console.log(id) // 1
   const todo = state.entities[id];
+  // console.log(todo) // {id: "1", name: "Redux Standalone with advanced Actions"}
   const toggoledTodo = { ...todo, completed: !todo.completed };
-  const entities = { ...state.entities, [id]: toggoledTodo };
+  // console.log(toggoledTodo) // {id: "1", name: "Redux Standalone with advanced Actions", completed: true}
+  const entities = { ...state.entities, [id]: toggoledTodo }; // id number where you want to write completed/incompleted
     return { ...state, entities };
 };
 
@@ -72,16 +119,16 @@ function applySetFilter(state, action) {
   return action.filter;
 }
 
-// action types
-
-const TODO_ADD = 'TODO_ADD';
-const TODO_TOGGLE = 'TODO_TOGGLE';
-const FILTER_SET = 'FILTER_SET';
+const rootReducer = combineReducers( 
+  { 
+    todoState:todoReducer,
+    filterState: filterReducer,
+    }
+);
 
 // //////////////////
 // action creators //
 // //////////////////
-
 function doAddTodo(id, name) {
   return {
     type: TODO_ADD,
@@ -106,12 +153,6 @@ function doSetFilter(filter) {
 // ////////
 // store //
 // ////////
-const rootReducer = combineReducers( 
-  { 
-    todoState:todoReducer,
-    filterState: filterReducer,
-    }
-);
 
 const logger = createLogger();
 
@@ -119,8 +160,8 @@ const store = createStore(
   rootReducer,
   undefined,
   applyMiddleware(logger)
-);
-
+  );
+  
 // /////////////
 // components //
 // /////////////
@@ -129,22 +170,39 @@ function TodoApp() {
 }
 
 function TodoList({ todosAsIds }) {
+  // console.log(todosAsIds)
+  //  ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
   return (
-    <div>
+    <div className="container" >
       {todosAsIds.map(todoId => <ConnectedTodoItem
         key={todoId}
-        todoId={todoId}
+        // sending in ids -> 1, 2, 3 ......
+        todoIIIId={todoId}
       />)}
     </div>
   );
 }
 
 function TodoItem( { todo, onToggleTodo } ) {
+  // console.log(todo)
+  /* 
+    {id: "1", name: "Redux Standalone with advanced Actions"}
+    {id: "2", name: "Redux Standalone with advanced Reducers"}
+    {id: "3", name: "Bootstrap App with Redux"}
+    {id: "4", name: "Naive Todo with React and Redux"}
+    {id: "5", name: "Sophisticated Todo with React and Redux"}
+    {id: "6", name: "Connecting State Everywhere"}
+    {id: "7", name: "Todo with advanced Redux"}
+    {id: "8", name: "Todo but more Features"}
+    {id: "9", name: "Todo with Notifications"}
+    {id: "10", name: "Hacker News with Redux"} 
+  */
   const { name, id, completed } = todo;
   return (
-    <div>
-      {name}
+    <div className={ completed ? "row line" : "row"} >
+      {name }
       <button
+        className="btn"
         type="button"
         onClick={() => onToggleTodo(id)}
       >
@@ -160,14 +218,48 @@ function TodoItem( { todo, onToggleTodo } ) {
 
 // conecting react and redux
 function mapStateToPropsList( state ){
+  console.log(state.todoState)
   return{
     todosAsIds: state.todoState.ids,
   }
 }
 
 function mapStateToPropsItem( state, props ){
+  // console.log(state)
+  /*  
+  {todoState: {…}, filterState: "SHOW_ALL"}
+  filterState: "SHOW_ALL"
+  todoState:
+    entities:
+      1: {id: "1", name: "Redux Standalone with advanced Actions"}
+      2: {id: "2", name: "Redux Standalone with advanced Reducers"}
+      3: {id: "3", name: "Bootstrap App with Redux"}
+      4: {id: "4", name: "Naive Todo with React and Redux"}
+      5: {id: "5", name: "Sophisticated Todo with React and Redux"}
+      6: {id: "6", name: "Connecting State Everywhere"}
+      7: {id: "7", name: "Todo with advanced Redux"}
+      8: {id: "8", name: "Todo but more Features"}
+      9: {id: "9", name: "Todo with Notifications"}
+      10: {id: "10", name: "Hacker News with Redux"}
+    ids: (10) ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+  */
+  // console.log(props)
+ /* 
+  {todoIIIId: "1"}
+  {todoIIIId: "2"}
+  {todoIIIId: "3"}
+  {todoIIIId: "4"}
+  {todoIIIId: "5"}
+  {todoIIIId: "6"}
+  {todoIIIId: "7"}
+  {todoIIIId: "8"}
+  {todoIIIId: "9"}
+  {todoIIIId: "10"}
+    
+  */
   return {
-    todo: state.todoState.entities[props.todoId],
+    // sending 1 id at one itteration
+    todo: state.todoState.entities[props.todoIIIId],
   }
 }
 
@@ -182,8 +274,9 @@ const ConnectedTodoItem = connect( mapStateToPropsItem, mapDispatchToPropsItem )
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>index.js</h1>
+    <Provider store={store}>
+      <TodoApp/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
